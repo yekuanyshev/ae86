461101b8ca8bcb30d8785ec9699dcca88d3ebf3a
supernova0730 <kuanysheveldar123@gmail.com>
2022-10-21 23:22:19 +0600

    small changes

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -1,25 +1,15 @@
 package postgres
 
 import (
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"log"
-	"os"
 )
-import "gorm.io/driver/postgres"
 
 func Connect(config Config) (*gorm.DB, error) {
 	dialector := postgres.Open(config.DSN())
 
-	gormLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags),
-		logger.Config{
-			Colorful: true,
-			LogLevel: logger.Info,
-		},
-	)
-
 	return gorm.Open(dialector, &gorm.Config{
-		Logger: gormLogger,
+		Logger: logger.Default.LogMode(logger.Info),
 	})
 }
